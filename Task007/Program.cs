2d8a8f27a1cbe2bd5b84174f83d711a8f46e06dd
Nofre417 <nofrevideo@gmail.com>
2023-08-02 12:44:48 +0200

    Задача. Возвести число от 1 до N в квадрат

---
+++ Task007/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите ваше число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    double value = Math.Pow(count, 2);
+    Console.Write($"{value} ");
+    count++;
+}
