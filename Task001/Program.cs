6e8666708cab7e664a40c54bee4375527814b266
Nofre417 <nofrevideo@gmail.com>
2023-08-02 11:06:38 +0200

    Задача по нахождению второй цифры в трехзначном числе

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int Message (string message)
+{
+    Write(message);
+    string value = ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+} 
+
+int number = Message("Введите трехзначное число > ");
+if (number < 100 || number > 1000)
+{
+    WriteLine("Вы ввели неверно число");
+    return;
+}
+WriteLine($"Введеное число = {number}");
+int secondnum = number / 10 % 10;
+WriteLine($"Вторая цифра числа = {secondnum}");
