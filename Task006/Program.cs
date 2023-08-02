c688fbd6de7696ce615571af29b571af9afc55e6
Nofre417 <nofrevideo@gmail.com>
2023-08-02 12:25:08 +0200

    Задача. Найти длину отрезка данных точек и их координат по формуле Пифагора

---
+++ Task006/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите значение X1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение X2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"Длина отрезка равна {d:f2}");
