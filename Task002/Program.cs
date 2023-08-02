e3dae4dfa68b8f89b74a59812002e92ee66af64f
Nofre417 <nofrevideo@gmail.com>
2023-08-02 11:22:32 +0200

    Задача по нахождению третьей цифры числа, если цифры нет вывести сообщение

---
+++ Task002/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int Message (string message)
+{
+    Write(message);
+    string value = ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+} 
+
+int number = Message("Введите число > ");
+if (number < 100)
+{
+    WriteLine("Третьей цифры нет");
+    return;
+}
+WriteLine($"Введеное число = {number}");
+
+int ThirdNum (int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+WriteLine($"Вторая цифра числа = " + ThirdNum(number));
