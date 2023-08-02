8500ecdb75b21a901f7db1630d5e695d2c696760
Nofre417 <nofrevideo@gmail.com>
2023-08-02 12:09:48 +0200

    Изменил алгоритм поиска значений через функцию SWITHC

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -5,8 +5,26 @@ Console.Clear();
 Console.Write("Введите номер четверти: ");
 int num = int.Parse(Console.ReadLine());
 
-
-if (num == 1) Console.WriteLine("x > 0, y > 0");
-if (num == 2) Console.WriteLine("x < 0, y > 0");
-if (num == 3) Console.WriteLine("x < 0, y < 0");
-if (num == 4) Console.WriteLine("x > 0, y < 0");
+switch (num)
+{
+    case 1: {
+        Console.WriteLine("x > 0, y > 0");
+        break;
+    }
+    case 2: {
+        Console.WriteLine("x < 0, y > 0");
+        break;
+    }
+    case 3: {
+        Console.WriteLine("x < 0, y < 0");
+        break;
+    }
+    case 4: {
+        Console.WriteLine("x > 0, y < 0");
+        break;
+    }
+    default :{
+        Console.WriteLine("Неверные данные");
+        break;
+    }
+}
