3779f349ff99290eb0f425418537ee723e4c0c4d
Nofre417 <nofrevideo@gmail.com>
2023-08-16 19:11:17 +0200

    Задача. Написать метод по созданию массива на 8 рандомных символах двоичного кода

---
+++ Task014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.Clear();
+
+Console.WriteLine(String.Join(".", GetNumsForArray(8)));
+
+//String.Join - Позволяет вывести строку с заданым сеператором ( знаком ).
+
+int[] GetNumsForArray(int size)
+{
+    int[] result = new int[size];
+    for(int j = 0; j < size; j++)
+    {
+        result[j] = new Random().Next(0,2);
+    }
+    return result;
+}
+
