1bf45395ef7dd8fbfe842f75b81af01431906c58
Nofre417 <nofrevideo@gmail.com>
2023-08-16 18:49:23 +0200

    Задача. Написать метод нахождения факториала заданного числа

---
+++ Task013/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+Console.WriteLine($"Факториал числа равен {GetFactorialOfNumber(int.Parse(Console.ReadLine()))}");
+
+int GetFactorialOfNumber (int number)
+{
+    int sum = 1;
+
+    for(int i = 1; i <= number; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
