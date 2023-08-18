19c83d54f1cfc6fd7de808e9da82e743f3dc2472
Nofre417 <nofrevideo@gmail.com>
2023-08-18 18:48:34 +0200

    Задача. Найти количество элементов которые лежат в отрезке от 10 до 99 включительно в массиве из 123 элементов

---
+++ Task021/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] array = GetArray(123, 0, 200);
+
+int count = 0;
+
+WriteLine(FindFitNums(array, count));
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+int FindFitNums(int[] array, int count)
+{
+    foreach(var item in array)
+    {
+        if(item >= 10 && item <= 99) 
+        {
+            Write(item +",");
+            count++;
+        }
+    }
+    WriteLine();
+    return count;
+}
