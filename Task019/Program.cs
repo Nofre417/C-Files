6c4c0b681b5d06e9208a77e06ee70bde4719c676
Nofre417 <nofrevideo@gmail.com>
2023-08-18 17:35:52 +0200

    Задача. Поменять знаки чисел в массиве на противоположные

---
+++ Task019/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] array = GetArray(6, -9, 10);
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+void InverseArray(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length; i++)
+    {
+        inArray[i] *= -1;
+    }
+}
+
+Write(String.Join(",", array));
+InverseArray(array);
+WriteLine();
+Write(String.Join(",", array));
