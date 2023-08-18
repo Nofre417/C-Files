e5e29d3f44ae5246cdc7c848fbd12f7ff29df140
Nofre417 <nofrevideo@gmail.com>
2023-08-18 18:27:35 +0200

    Задача. Ввести массив и найти совпадение элемента при моиске

---
+++ Task020/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите через пробел элемерты масива: ");
+int[] array = GetArrayFromString(ReadLine());
+
+Write("Введите элемент для поиска: ");
+int element = int.Parse(ReadLine());
+
+if(FindElement(array, element))
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");   
+}
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for(int i = 0; i <result.Length; i++) 
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
+
+bool FindElement(int[] inArray, int element)
+{
+    foreach(var item in inArray)
+    {
+        if(item == element) return true;
+    }
+    return false;
+}
