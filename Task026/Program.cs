6c760d8cc4d00921c05682b345d6ab7654c456b1
Nofre417 <nofrevideo@gmail.com>
2023-08-23 22:08:27 +0200

    Задача. Создать массив и сделать реверс всех элементов в нем (2 способа)

--- Task026/Program.cs
+++ Task026/Program.cs
@@ -7,10 +7,12 @@ Write("Введите длину массива: ");
 int n = int.Parse(ReadLine());
 
 int[] array1 = GetArray(n, 0, 20);
-int[] array2 = ArrayReverse(array1);
+int[] array2 = ArrayReverse1(array1);
 
-WriteLine(String.Join(" ", array1));
-WriteLine(String.Join(" ", array2));
+WriteLine("Массив: " + String.Join(" ", array1));
+WriteLine("Первый сопособ: " + String.Join(" ", array2));
+ArrayReverse2(array1);
+WriteLine("Второй сопособ: " + String.Join(" ", array1));
 
 int[] GetArray(int size, int min, int max)
 {
@@ -23,7 +25,7 @@ int[] GetArray(int size, int min, int max)
     return result;
 }
 
-int[] ArrayReverse(int[] inArray)
+int[] ArrayReverse1(int[] inArray)
 {
     int[] result = new int[inArray.Length];
     for(int i = 0; i < inArray.Length; i++)
@@ -31,4 +33,14 @@ int[] ArrayReverse(int[] inArray)
         result[i] = inArray[inArray.Length - 1 - i];
     }
     return result;
+}
+
+void ArrayReverse2(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length/2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = k;
+    }
 }
