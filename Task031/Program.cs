64f23bdec248e83f4e2483d5e68385c00f8ec87a
Nofre417 <nofrevideo@gmail.com>
2023-08-24 00:24:55 +0200

    Задача ДЗ. Ввести элементы массива и посчитать сколько из них положительных

---
+++ Task031/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите через пробел элемерты масива: ");
+int[] array = GetArrayFromString(ReadLine());
+GetAmontPositive(array);
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] num = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[num.Length];
+    for(int i = 0; i <result.Length; i++) 
+    {
+        result[i] = int.Parse(num[i]);
+    }
+    return result;
+}
+
+void GetAmontPositive(int[] array)
+{
+    int result = 0;
+    foreach(var item in array)
+    {
+        if(item > 0) result++;
+    }
+    WriteLine($"Количество положительных числе: {result}");
+}
