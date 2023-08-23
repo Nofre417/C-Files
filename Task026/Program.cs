7920cdac6818f7d731479e63e93d18a8361efc65
Nofre417 <nofrevideo@gmail.com>
2023-08-23 21:48:44 +0200

    Задача. Создать массив и перевернуть элементы в нем ( реверс )

---
+++ Task026/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите длину массива: ");
+int n = int.Parse(ReadLine());
+
+int[] array1 = GetArray(n, 0, 20);
+int[] array2 = ArrayReverse(array1);
+
+WriteLine(String.Join(" ", array1));
+WriteLine(String.Join(" ", array2));
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+int[] ArrayReverse(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for(int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
