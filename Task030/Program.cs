cb0e4c5ad544737b14dcb39ff53c84bb18d4aea4
Nofre417 <nofrevideo@gmail.com>
2023-08-24 00:01:30 +0200

    Задача. Ввести/Создать массив и скопировать его поэллементно

---
+++ Task030/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using static System.Console;
+
+Clear();
+
+Write("Введите размер массива: ");
+int size = int.Parse(ReadLine());
+
+int[] array1 = GetArray(0, 100, size);
+Write("Старый массив: " + String.Join(" ", array1));
+
+WriteLine();
+
+int[] array2 = CopyArray(array1, size);
+Write("Новый массив: " + String.Join(" ", array2));
+
+
+int[] GetArray(int min, int max, int size)
+{
+    int[] result = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+
+int[] CopyArray(int[] oldArray, int size)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = oldArray[i];
+    }
+    return newArray;
+}
