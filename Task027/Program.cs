220f6dfec0542602f2578b7517aa0759d1dddd69
Nofre417 <nofrevideo@gmail.com>
2023-08-23 22:24:27 +0200

    Задача. Ввести 3 значения и выяснить существует ли треугольник с данными сторонами

---
+++ Task027/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите сторону A: ");
+int a = int.Parse(ReadLine());
+Write("Введите сторону B: ");
+int b = int.Parse(ReadLine());
+Write("Введите сторону C: ");
+int c = int.Parse(ReadLine());
+
+if(FindTriangle(a, b, c)) WriteLine("Ответ: Истина!");
+else WriteLine("Ответ: Ложь!"); 
+
+
+bool FindTriangle(int A, int B, int C)
+{
+    bool result = false;
+
+    if(A + B > C && A + C > B && B + C > A) result = true;
+    else result = false;
+
+    return result;
+}
