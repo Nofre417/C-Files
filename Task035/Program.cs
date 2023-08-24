e42e7a04daad4911c014a39e501e8b15c794d760
Nofre417 <nofrevideo@gmail.com>
2023-08-24 03:03:56 +0200

    Создать двухмерный массив и найти сумму элементов по диагонали (0,0) + (1, 1)...

---
+++ Task035/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml.XPath;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество стобцов массива: ");
+int colons = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, colons);
+
+WriteLine("Созданный массив: ");
+PrintArray(array);
+
+int summa = ToFindSumOfArray(array);
+WriteLine($"Сумма элементов по диагонали: {summa}");
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colons; j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine(" ");
+    }
+}
+int[,] GetArray(int rows, int colons)
+{
+    int[,] result = new int[rows, colons];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colons; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+int ToFindSumOfArray(int[,] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < colons; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if(i == j) sum+= array[i, j];
+        }
+    }
+    return sum;
+}
